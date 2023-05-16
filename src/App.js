049e5560ffbff4c999cfa97d5408773a706c7ea3
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-05-16 15:44:23 -0400

    Fetch quotes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
-import FavoriteQuotes from "./components/quotes/FavoriteQuotes";
-import CategoryForm from "./components/quotes/CategoryForm";
-import FilteredQuotes from "./components/quotes/FilteredQuotes";
-import Message from "./components/Message";
 import { Loader } from "react-feather";
-import RandomQuote from "./components/quotes/RandomQuote";
 import "./App.css";
 
 function App() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [favoriteQuotes, setFavoriteQuotes] = useState(JSON.parse(window.localStorage.getItem("favoriteQuotes")) || []);
-  const [showMessage, setShowMessage] = useState(false);
-  const [messageText, setMessageText] = useState("");
-  const [category, setCategory] = useState("all");
-  const [randomQuote, setRandomQuote] = useState("");
-  const categories = ["all", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
   const quotesUrl =
     "https://gist.githubusercontent.com/redrambles/d50714387b93d7fe3e78b346c158719e/raw/254337559896fd3b8e288e394f337ac098ed5cbb/quotes.js";
 
@@ -31,8 +20,6 @@ function App() {
       setQuotes(results);
     } catch (error) {
       console.log("There was an error", error);
-      setMessageText("Sorry - no Quotes available. Check your online connection.");
-      setShowMessage(true);
     }
     setLoading(false);
   };
@@ -41,70 +28,11 @@ function App() {
     fetchQuotes();
   }, []);
 
-  useEffect(() => {
-    setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)]);
-  }, [quotes]);
-
-  useEffect(() => {
-    window.localStorage.setItem("favoriteQuotes", JSON.stringify(favoriteQuotes));
-  }, [favoriteQuotes]);
-
-  // Whenever either the 'category' or 'quotes' state values change,
-  // the component re-renders and 'filteredQuotes' is re-calculated.
-  const filteredQuotes = category !== "all" ? quotes.filter((quote) => quote.categories.includes(category)) : quotes;
-
-  const handleCategoryChange = (e) => {
-    setCategory(e.target.value);
-  };
-
-  const displayAnotherRandomQuote = () => {
-    setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)]);
-  };
-
-  const removeFromFavorites = (quoteId) => {
-    const updatedFavorites = favoriteQuotes.filter((quote) => quote.id !== quoteId);
-    setFavoriteQuotes(updatedFavorites);
-  };
-
-  const addToFavorites = (quoteId) => {
-    const selectedQuote = quotes.find((quote) => quote.id === quoteId);
-    if (favoriteQuotes.length >= MAXFAVES) {
-      setShowMessage(true);
-      setMessageText("Max number of Favorite Quotes reached. Please delete one to add another!");
-    } else {
-      const existingQuote = favoriteQuotes.find((quote) => quote.id === quoteId);
-      if (existingQuote) {
-        setShowMessage(true);
-        setMessageText("This quote is already in your favorites! Choose another.");
-      } else {
-        setShowMessage(true);
-        setMessageText("Added to Favorites! :)");
-        setFavoriteQuotes([...favoriteQuotes, selectedQuote]);
-      }
-    }
-  };
-
   return (
     <div className='App'>
-      {showMessage && <Message messageText={messageText} setShowMessage={setShowMessage} />}
       <Header />
-
-      <main>
-        <FavoriteQuotes favoriteQuotes={favoriteQuotes} maxFaves={MAXFAVES} removeFromFavorites={removeFromFavorites} />
-        {randomQuote && (
-          <RandomQuote
-            addToFavorites={addToFavorites}
-            randomQuote={randomQuote}
-            displayAnotherRandomQuote={displayAnotherRandomQuote}
-          />
-        )}
-        <CategoryForm categories={categories} handleCategoryChange={handleCategoryChange} category={category} />
-        {loading ? (
-          <Loader />
-        ) : (
-          <FilteredQuotes category={category} filteredQuotes={filteredQuotes} addToFavorites={addToFavorites} />
-        )}
-      </main>
+      {loading ? <Loader /> : JSON.stringify(quotes)}
+      <main></main>
     </div>
   );
 }
