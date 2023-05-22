c8b01ef57b5b0907d527a3c1511016e76024ae47
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-05-22 15:35:34 -0400

    Inform how many quotes we have

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,10 +1,16 @@
 import React from "react";
 import QuoteCard from "./QuoteCard";
 
-function Quotes({ filteredQuotes, addToFavorites }) {
+function Quotes({ filteredQuotes, addToFavorites, category }) {
   return (
     <section>
       <div className='quotes'>
+        {
+          <p>
+            You have {filteredQuotes.length} great {category !== "all" && category}{" "}
+            {filteredQuotes.length === 1 ? "quote" : "quotes"}.
+          </p>
+        }
         {filteredQuotes.map((quote) => (
           <QuoteCard key={quote.id} quote={quote} addToFavorites={addToFavorites} />
         ))}
