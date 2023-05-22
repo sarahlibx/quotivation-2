530d290543e1ddf4a1f260b9d2bece5d18d842dd
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-05-22 15:22:58 -0400

    Message disappears after short delay

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Message = ({ messageText, removeMessage }) => {
+  useEffect(() => {
+    const messageTimeout = setTimeout(() => {
+      removeMessage();
+    }, 1500);
+    window.setTimeout(messageTimeout);
+
+    return () => window.clearTimeout(messageTimeout);
+  });
   return (
     <div className='message'>
       {messageText}{" "}
