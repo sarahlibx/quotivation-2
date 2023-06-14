5c7359fed078a149cbc4740db9fcac71b94f7ed9
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-06-14 16:18:38 -0400

    Update quote api link

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   const [messageText, setMessageText] = useState("");
   const [favoriteQuotes, setFavoriteQuotes] = useState(JSON.parse(window.localStorage.getItem("favoriteQuotes")) || []);
   const quotesUrl =
-    "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/61eba3576351b30987c4af1fadb2619af5fc4064/quotes.js";
+    "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
   const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
 
   const MAXFAVES = 3;
@@ -28,7 +28,7 @@ function App() {
       setLoading(true);
       const response = await fetch(quotesUrl);
       const results = await response.json();
-
+      console.log(results);
       setQuotes(results);
     } catch (error) {
       console.log("There was an error", error);
