8979245b1582f4c83b61d6fbad7ab47fca2a4881
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-06-19 17:30:55 -0400

    Modify copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className='wrapper'>Copyright 2023</footer>;
+  return <footer className='wrapper'>&copy; 2023 Quotivation</footer>;
 };
 
 export default Footer;
