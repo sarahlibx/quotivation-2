a1257608001cbad1a8dd8bc249d4f83e952d559f
sarah_s <reed.se01@gmail.com>
2024-06-19 11:53:52 -0400

    Initial set up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Loader } from "react-feather";
 import "./App.css";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(false);
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
+
+  const fetchQuotes = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(quotesUrl);
+      const results = await response.json();
+      setQuotes(results);
+    } catch (error) {
+      console.log("There was an error", error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
 
   return (
     <div className='App'>
       <Header />
-      <main></main>
+      <main>{loading ? <Loader /> : JSON.stringify(quotes)}</main>
       <Footer />
     </div>
   );
